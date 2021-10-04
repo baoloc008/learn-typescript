85df4f217ae87c643455a2a3a3f7f73b1ca43640
baoloc008 <baoloc008@gmail.com>
2021-10-04 16:37:04 +0700

    array

---
+++ array.ts
@@ -0,0 +1,12 @@
+let names = ["mai", "nam", "hung"];
+names.push("son");
+// names.push(2); // error
+
+let numbers = [1, 2, 3];
+// numbers.push("Loc"); // error
+
+let mixed = [1, "Loc", false];
+mixed.push(5);
+mixed.push("Bao Loc");
+mixed.push(true);
+// mixed.push({}); // error
