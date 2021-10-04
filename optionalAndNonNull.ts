be703105f131f77f03c32482f3be4451a1d91814
baoloc008 <baoloc008@gmail.com>
2021-10-04 19:27:35 +0700

    optional and non null

---
+++ optionalAndNonNull.ts
@@ -0,0 +1,11 @@
+export {};
+
+const add = (a: number, b?: number) => (b ? a + b : a);
+
+console.log(add(1, 2));
+
+console.log(add(3));
+
+const addNonNull = (a: number, b?: number) => a + b!;
+
+console.log(addNonNull(5));
