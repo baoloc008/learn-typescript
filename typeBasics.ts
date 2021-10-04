9abfd62eee593be5d46383d42341be4146047ffb
baoloc008 <baoloc008@gmail.com>
2021-10-04 16:06:25 +0700

    type basic

---
+++ typeBasics.ts
@@ -0,0 +1,12 @@
+let myName = "Loc";
+let age = 10;
+let isStudent = true;
+
+// myName = 20; // error
+myName = "Bao Loc";
+
+// age = "abc"; // error
+age = 15;
+
+// isStudent = "def"; // error
+isStudent = false;
