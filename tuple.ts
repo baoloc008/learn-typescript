871dd763b3e05a83ca4f75edadbafa557dd78aaa
baoloc008 <baoloc008@gmail.com>
2021-10-05 11:38:26 +0700

    tuple

---
+++ tuple.ts
@@ -0,0 +1,10 @@
+let arr = ["Loc", 10, false];
+arr[0] = 25;
+arr[1] = "Loc";
+arr[2] = "true";
+
+console.log(arr);
+
+const tup: [string, boolean, number] = ["Loc", true, 10];
+
+const myStudent: [string, number] = ["Loc", 10];
